f5dfe48155d5aa094e2f579db0e1e611072e872c
FFFFF <woshitiancai1014@gmail.com>
2025-03-22 15:38:21 +0800

    添加答卷微服务、Excel导出、答题和自动判卷

--- apps/answer/src/answer.service.ts
+++ apps/answer/src/answer.service.ts
@@ -1,8 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/prisma';
+import { Inject, Injectable } from '@nestjs/common';
+import { AnswerAddDto } from './dto/answer-add.dto';
 
 @Injectable()
 export class AnswerService {
-  getHello(): string {
-    return 'Hello Answer!';
+  @Inject(PrismaService)
+  private prismaService: PrismaService;
+
+  async add(answerAddDto: AnswerAddDto, userId: number) {
+    const exam = await this.prismaService.exam.findUnique({
+      where: {
+        id: answerAddDto.examId,
+      },
+    });
+
+    console.log('=====', exam);
+
+    let questions = [];
+    try {
+      questions = JSON.parse(exam?.content);
+    } catch (e) {}
+
+    let answers = [];
+    try {
+      answers = JSON.parse(answerAddDto.content);
+    } catch (error) {}
+
+    console.log('------', questions, answers);
+
+    let totalScore = 0;
+
+    answers.forEach((answer) => {
+      const question = questions.find((item) => item.id === answer.id);
+
+      if (
+        JSON.stringify(answer.answer) === JSON.stringify(question.userAnswer)
+      ) {
+        totalScore += question.score;
+      }
+    });
+
+    return this.prismaService.answer.create({
+      data: {
+        content: answerAddDto.content,
+        score: totalScore,
+        answerer: {
+          connect: {
+            id: userId,
+          },
+        },
+        exam: {
+          connect: {
+            id: answerAddDto.examId,
+          },
+        },
+      },
+    });
+  }
+
+  async list(examId: number) {
+    return this.prismaService.answer.findMany({
+      where: {
+        examId,
+      },
+      include: {
+        exam: true,
+        answerer: true,
+      },
+    });
+  }
+
+  async find(id: number) {
+    return this.prismaService.answer.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        exam: true,
+        answerer: true,
+      },
+    });
   }
 }
